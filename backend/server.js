087b211c6434e69d729f20e6c5857b5bca0dcd1f
talba1 <talhabas2003@hotmail.com>
2022-12-14 21:16:52 +0200

    14.12

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server at http:/localhost:${port}`);
+});
