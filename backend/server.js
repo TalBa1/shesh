306c5ad7ed51204b607f4e0ae22d600dda81df3c
TalBa1 <69452024+TalBa1@users.noreply.github.com>
2023-01-11 15:44:06 +0200

    s

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,42 @@
 import express from 'express';
-import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGODB_URI)
+const uri = process.env.MONGODB_URI;
+if(!uri) {
+  console.error("MONGODB_URI is not defined in .env file");
+  process.exit(1);
+}
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(uri, options)
   .then(() => {
     console.log('connected to db');
   })
   .catch((err) => {
     console.log(err.message);
+    process.exit(1);
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message })
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`server at http:/localhost:${port}`);
+  console.log(`server at http://localhost:${port}`);
 });
